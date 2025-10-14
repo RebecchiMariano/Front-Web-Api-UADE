c78def534d8c6a0c5a39168fc9701269d7016b5e
IamRoman-rb <roman.borla2004@gmail.com>
2025-10-14 14:17:04 -0300

    Ordenando directorios y archivos

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
+
 import App from './App.jsx'
 import Home from './pages/Home/Home.jsx'
 import AboutUs from './pages/AboutUs/AboutUs.jsx'
-
+import Login from './pages/Login/Login.jsx';
+import Checkout from './pages/DatosTarjeta/Checkout.jsx'
 
 
 createRoot(document.getElementById('root')).render(
@@ -14,6 +16,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/checkout' element={ <Checkout />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
